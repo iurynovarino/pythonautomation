c793c58e48ec37ddadc876da44ed0377d5c71020
iurynovarino <novarinos@hotmail.com>
2025-08-29 14:17:49 -0300

    Commit translating coments and change of variables from RG.py

--- RG.py
+++ RG.py
@@ -1,22 +1,22 @@
 from azure.identity import DefaultAzureCredential
 from azure.mgmt.resource import ResourceManagementClient
 
-# Parâmetros de entrada
-subscription_id = "<sua_subscription_id>"
-resource_group_name = "rg_teste"
-location = "brazilsouth"
+# Variables declaration
+subscription_id = "<YOUR_SUBSCRIPTION_ID>"  # Change to your subscription ID
+resource_group_name = "<YOUR_RESOURCE_GROUP>"  # Change to your resource group name
+location = "<YOUR_LOCATION>"  # Change to your desired location, e.g., "brazilsouth"
 
-# Autenticação usando o Azure CLI ou Managed Identity
+# Autenticating using the Azure Cli or Managed Identity
 credential = DefaultAzureCredential()
 
-# Inicializa o cliente de Resource Management
+# Start Resource Management client
 resource_client = ResourceManagementClient(credential, subscription_id)
 
-# Cria (ou atualiza) o grupo de recursos
+# Create or update the resource group
 resource_group_params = {"location": location}
 result = resource_client.resource_groups.create_or_update(
     resource_group_name, resource_group_params
 )
 
-# Exibe o resultado
+# Show result
 print(f"Grupo de recurso '{result.name}' criado com sucesso em '{result.location}'")
