a43fdc6cd5d7850bbd0abcef2c4d4ced868f30ba
iurynovarino <novarinos@hotmail.com>
2025-08-29 14:35:19 -0300

    Commit translating coments and change of variables from Aks.py

--- Aks.py
+++ Aks.py
@@ -8,23 +8,23 @@ from azure.mgmt.containerservice.models import (
     ManagedClusterIdentity
 )
 
-# Parâmetros
-subscription_id = "sua subscription_id"
-resource_group = "rg_teste"
-location = "brazilsouth"
-aks_name = "arck-teste-hml"
-subnet_id = "/subscriptions/sua subscription_id/resourceGroups/rg_teste/providers/Microsoft.Network/virtualNetworks/vnet_teste-hml/subnets/sub_arck_teste"  # Formato: /subscriptions/.../subnets/...
-node_vm_size = "Standard_DS2_v2"
-dns_name_prefix = "arck-teste-hml-dns"
-dns_service_ip = "10.240.6.5"         # Ex: 10.2.0.10
-service_cidr = "10.240.6.0/24"                # Ex: 10.2.0.0/16
-acr_id = "acrtestehml"                # /subscriptions/.../resourceGroups/.../providers/Microsoft.ContainerRegistry/registries/...
+# Parameters
+subscription_id = "<subscription_id>"  # Change to your subscription ID"
+resource_group = "<resource_group>"  # Change to your resource group name
+location = "<location>"  # Change to your desired location, e.g., "brazilsouth"
+aks_name = "<aks_cluster_name>"  # Change to your desired AKS cluster name (must be unique)
+subnet_id = "/subscriptions/sua subscription_id/resourceGroups/rg_teste/providers/Microsoft.Network/virtualNetworks/<aks_cluster_name>/subnets/sub_arck_teste"  # Formato: /subscriptions/.../subnets/...
+node_vm_size = "Standard_DS2_v2" 
+dns_name_prefix = "<dns_prefix>"  # Change to your desired DNS prefix (must be unique)
+dns_service_ip = "<dns_service_ip>"  # Ex: "         # Ex: 10.2.0.10
+service_cidr = "<service_cidr>"                # Ex: 10.2.0.0/16
+acr_id = "<acr_id>"                # /subscriptions/.../resourceGroups/.../providers/Microsoft.ContainerRegistry/registries/...
 
-# Inicializa client
+# Start client
 credential = DefaultAzureCredential()
 aks_client = ContainerServiceClient(credential, subscription_id)
 
-# Define perfil de agente (nó)
+# Define agent profile (node)
 agent_pool_profile = ManagedClusterAgentPoolProfile(
     name="nodepool1",
     count=1,
@@ -35,14 +35,14 @@ agent_pool_profile = ManagedClusterAgentPoolProfile(
     vnet_subnet_id=subnet_id
 )
 
-# Define perfil de rede
+# set network profile
 network_profile = ContainerServiceNetworkProfile(
     network_plugin="azure",
     dns_service_ip=dns_service_ip,
     service_cidr=service_cidr
 )
 
-# Define cluster
+# set the cluster
 cluster = ManagedCluster(
     location=location,
     dns_prefix=dns_name_prefix,
@@ -56,7 +56,7 @@ cluster = ManagedCluster(
     aad_profile=None
 )
 
-# Criar cluster AKS
+# Creating cluster AKS
 print(f"Criando cluster AKS '{aks_name}'...")
 poller = aks_client.managed_clusters.begin_create_or_update(
     resource_group_name=resource_group,
@@ -66,8 +66,8 @@ poller = aks_client.managed_clusters.begin_create_or_update(
 result = poller.result()
 print(f"Cluster AKS '{aks_name}' criado com sucesso.")
 
-# Anexar o ACR ao AKS (requer permissões extras se feito via código)
-# Alternativa via Azure CLI: az aks update -n $aks --attach-acr $acr
+# Attach the ACR to AKS (requires extra permissions if done via code)
+# Alternative via Azure CLI: az aks update -n $aks --attach-acr $acr
 print(f"Anexando ACR '{acr_id}' ao cluster...")
 aks_client.managed_clusters.begin_list_cluster_admin_credentials(resource_group, aks_name)
 aks_client.managed_clusters.begin_attach_acr(resource_group, aks_name, acr_id)  # apenas em versões com suporte
